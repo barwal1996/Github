cf0ae1c9cae9ce736ba10d7cf15b5c66c7ba84b6
Training <Training@192.168.252.212>
2020-03-07 14:46:55 +0530

    its a next version

--- GitPlayground/src/HelloGit.java
+++ GitPlayground/src/HelloGit.java
@@ -3,5 +3,7 @@ public class HelloGit {
 	public static void main(String[] args)
 	{
 		System.out.println("Hello Git, I am here. Save us");
+		System.out.println("Bharat mata ki jai");
+		System.out.println("Jai bhole naat ki ");
 	}
 }
